785e93edd3d739a03b61125a7fa136c5d0484e8f
Ajo <ajoeldhobaby@gmail.com>
2021-05-07 23:46:08 +0530

    Converted to function. Found number of projects

--- module/Automate.py
+++ module/Automate.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import date
 
+driver = webdriver.Chrome(driverPath)
+
 def WaitTillElementFound(element):
     try:
         return WebDriverWait(driver, 10).until(
@@ -14,40 +16,68 @@ def WaitTillElementFound(element):
     except :
         driver.quit()
 
-driver = webdriver.Chrome(driverPath)  # Optional argument, if not specified will search path.
-driver.get('http://3.7.23.170/');
+def GetDriver():
+    driver.get('http://3.7.23.170/');
 
-usernameField = WaitTillElementFound("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]")
-usernameField.send_keys(username)
-passwordField = driver.find_element_by_xpath("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[2]/input[1]")
-passwordField.send_keys(password)
-SubmitButton = driver.find_element_by_xpath("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[3]/input[1]")
-SubmitButton.click() 
+def Login():
+    usernameField = WaitTillElementFound("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]")
+    usernameField.send_keys(username)
+    passwordField = driver.find_element_by_xpath("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[2]/input[1]")
+    passwordField.send_keys(password)
+    SubmitButton = driver.find_element_by_xpath("/html[1]/body[1]/div[4]/div[1]/div[1]/form[1]/div[2]/div[3]/input[1]")
+    SubmitButton.click() 
 
-timeButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/ul[1]/li[6]/a[1]/b[1]")
-timeButton.click()
-enterTimeButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[4]/div[1]/ul[1]/li[2]/a[1]/p[1]")
-enterTimeButton.click()
+def NavigateToLogTime():
+    timeButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/ul[1]/li[6]/a[1]/b[1]")
+    timeButton.click()
+    enterTimeButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[4]/div[1]/ul[1]/li[2]/a[1]/p[1]")
+    enterTimeButton.click()
 
-day = date.today().weekday()
-index = "";
-if(day == 6):
-    dayIndex = 2;
-else:
-    dayIndex = day + 3
-index = dayIndex
-timeBoxHour = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[5]/div[1]/div[1]/table[1]/tbody[3]/tr[1]/td["+str(index) +"]/input[1]")
-timeBoxHour.click()
-timeSplitted = timevalue.split(':')
-timeBoxHour.send_keys(timeSplitted[0])
-timeBoxHour.send_keys(Keys.TAB)
-timeBoxHour.send_keys(timeSplitted[1])
-submitButton = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[5]/div[1]/div[1]/table[1]/tbody[4]/tr[3]/td[2]/div[1]/input[1]")
-submitButton.click()
-profileButton = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/span[1]")
-profileButton.click()
-logoutButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[3]")
-logoutButton.click()
+def FindNumberOfProjects():
+    header = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[5]/div[1]/div[1]/table[1]/tbody[3]")
+    numberOfProjects = header.find_elements_by_tag_name("tr")
+    return str(len(numberOfProjects))
 
-time.sleep(2)
-driver.quit()
+
+def EnterTime():
+    day = date.today().weekday()
+    index = "";
+    if(day == 6):
+        dayIndex = 2;
+    else:
+        dayIndex = day + 3
+    index = dayIndex
+    timeBoxHour = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[5]/div[1]/div[1]/table[1]/tbody[3]/tr[1]/td["+str(index) +"]/input[1]")
+    timeBoxHour.click()
+    timeSplitted = timevalue.split(':')
+    timeBoxHour.send_keys(timeSplitted[0])
+    timeBoxHour.send_keys(Keys.TAB)
+    timeBoxHour.send_keys(timeSplitted[1])
+
+def LogOut():
+    body = driver.find_element_by_css_selector('body')
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(1)
+    submitButton = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[5]/div[1]/div[1]/table[1]/tbody[4]/tr[3]/td[2]/div[1]/input[1]")
+    submitButton.click()
+    body.send_keys(Keys.PAGE_UP)
+    time.sleep(1)
+    profileButton = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/span[1]")
+    profileButton.click()
+    logoutButton = WaitTillElementFound("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[3]")
+    logoutButton.click()
+
+def QuitDriver(): 
+    time.sleep(5)
+    driver.quit()
+
+GetDriver()
+Login()
+NavigateToLogTime()
+FindNumberOfProjects()
+EnterTime()
+LogOut()
+QuitDriver()
+
+
+
